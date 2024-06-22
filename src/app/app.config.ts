69a0cd296600c27ed1f785d91d48baba1cdec261
Thales Maia <thalesmaiaalexandre@gmail.com>
2024-06-22 10:38:54 -0300

    feat: Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,21 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient  } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr(({
+      positionClass: 'toast-top-center',
+      preventDuplicates: true,
+    }))
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './pages/users/users.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/users',
+    pathMatch: 'full',
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+  }, 
+  {
+    path: 'users/new',
+    component: NewUserComponent,
+  },
+  {
+    path: 'users/:id',
+    component: NewUserComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/users',
+  }
+];
